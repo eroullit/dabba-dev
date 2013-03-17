23a6fa2491a48ea09c531fd143e3260007381a04
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2013-03-17 18:25:55 +0100

    make sure all packages are installed on target.
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- cookbooks/dabba/recipes/default.rb
+++ cookbooks/dabba/recipes/default.rb
@@ -4,6 +4,13 @@ directory "/vcs/git/dabba" do
   recursive true
 end
 
+# Install dependencies
+%w{build-essential git cmake python-yaml libprotobuf-c0-dev protobuf-c-compiler libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libcap2-bin}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
 # Clone project sources
 git "dabba" do
   repository "git://github.com/eroullit/dabba.git"
